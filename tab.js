05a997d7e56dde9a456e61a78f3240078f8d5900
pilstar <110075151+jeonpilseong@users.noreply.github.com>
2023-02-19 20:20:20 +0900

    Add files via upload

---
+++ tab.js
@@ -0,0 +1,17 @@
+
+//for(let i = 0; i < $('.tab-button').length; i++) {
+//	$('.tab-button').eq(i).on('click', function() {
+//		탭열기(i);
+//	});
+//}
+
+$('.list').click(function(e) {
+	탭열기(e.target.dataset.id);
+})
+
+function 탭열기(숫자) {
+	$('.tab-button').removeClass('orange');
+	$('.tab-button').eq(숫자).addClass('orange');
+	$('.tab-content').removeClass('show');
+	$('.tab-content').eq(숫자).addClass('show');
+}
